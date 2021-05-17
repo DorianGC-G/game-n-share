712577d67c1c887b63108623b58cc51502df6f86
Dorian Guitel <dorian.guitel@gmail.com>
2021-05-17 17:14:19 +0200

    finished show and index for offers controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Offer.destroy_all
+User.destroy_all
+clement = User.create!(username: "Clément Le Moigne", password: "weshgros69", email: "clement@weshgros.com")
+Offer.create!(title: "PS4 à louer", item: "ps4", description: "dispo à paris du 4 mars au 34 avril", price_per_day: 400, user_id: clement.id)
+Offer.create!(title: "PS5 à louer", item: "ps5", description: "dispo à paris du 5 mars au 51 avril", price_per_day: 9900, user_id: clement.id)
