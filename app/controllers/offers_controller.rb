f9ef507f817a2b8ed285e70b9d6244e5df4f904c
Manon Schnetzler <bonsoir@mailo.com>
2021-05-18 19:07:21 +0200

    Il est maintenant impossible de supprimer et modifier les offres que l'on a pas créées

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -31,15 +31,15 @@ class OffersController < ApplicationController
 
   def update
     @offer = Offer.find(params[:id])
-    @offer.update(offer_params)
     authorize @offer
+    @offer.update(offer_params)
     redirect_to offer_path(@offer)
   end
 
   def destroy
     @offer = Offer.find(params[:id])
-    @offer.destroy
     authorize @offer
+    @offer.destroy
     redirect_to offers_path
   end
 
