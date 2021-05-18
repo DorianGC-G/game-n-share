06bc390a5eb58abc8b4149a5ff50d6f7790615fa
Dorian Guitel <dorian.guitel@gmail.com>
2021-05-18 16:47:27 +0200

    added forms front and routed stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'offers/index', to: 'offers#index'
-  get 'offers/:id', to: 'offers#show'
+  resources :offers, only: [:new, :create, :index]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
