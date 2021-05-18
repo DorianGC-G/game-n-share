88820e8a708dcb8da29e76f439dc06d1238ca708
Manon Schnetzler <bonsoir@mailo.com>
2021-05-18 21:07:15 +0200

    formulaire de recherche sur homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @offers = Offer.all
+    # @offers = Offer.all
+
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR item ILIKE :query OR description ILIKE :query OR location ILIKE :query"
+      @offers = Offer.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @offers = Offer.all
+    end
   end
 end
