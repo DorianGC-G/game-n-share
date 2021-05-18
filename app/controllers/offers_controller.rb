d9e1c6b44dd4c7c2cc29bc44d0400a3800005c30
Manon Schnetzler <bonsoir@mailo.com>
2021-05-18 18:42:53 +0200

    Create ok

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -16,6 +16,7 @@ class OffersController < ApplicationController
   def create
     @offer = Offer.new(offer_params)
     authorize @offer
+    @offer.user = current_user
     if @offer.save
       redirect_to offer_path(@offer)
     else
