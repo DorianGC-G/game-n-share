f61b56b51dca23aa0e9e9e6f232946b01e326d81
Agathe Auriol <agathe.auriol@gmail.com>
2021-05-19 12:14:23 +0200

    method dashboard créée dans pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
+  def dashboard
+    @offers = policy_scope(Offer).where(user_id: current_user.id)
+  end
+
   def home
     @offers = Offer.all
 
