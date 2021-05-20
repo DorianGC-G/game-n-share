8285ad965fee861fff638f07965576668d35d9fe
Dorian Guitel <dorian.guitel@gmail.com>
2021-05-20 17:15:15 +0200

    functionnal booking cards with status

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,9 +13,21 @@ class BookingsController < ApplicationController
     end
   end
 
+  def update
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    if params[:accept] == "accepted"
+      @booking.status = true
+    elsif params[:accept] == "declined"
+      @booking.status = false
+    end
+    @booking.save
+    redirect_to dashboard_path
+  end
+
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 end
