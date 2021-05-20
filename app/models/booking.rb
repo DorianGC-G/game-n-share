55498d1eac45076347deaadd179d1e24ea7c29d3
clement le moigne <clement.lemoigne@gmail.com>
2021-05-20 16:51:03 +0200

    stand by

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :offer
   belongs_to :user
+  validate :end_date_after_start_date
+  validates :start_date, :end_date, :overlap => true
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    errors.add(:end_date, "must be after the start date") if end_date < start_date
+  end
 end
